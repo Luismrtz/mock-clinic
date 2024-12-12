4cf4368714fba40449ac959720a7d491f30e6140
Luis Martinez <Luisricardo494@gmail.com>
2024-12-12 13:57:09 -0700

    Add e2e and component test support to github actions (#2)

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,20 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    baseUrl: "http://localhost:3000",
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    supportFile: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+  },
+component: {
+  supportFile: "cypress/support/component.ts",
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
+    },
+    indexHtmlFile: "cypress/support/component-index.html",
+    specPattern: "cypress/component/**/*.cy.{js,jsx,ts,tsx}",
   },
 });
